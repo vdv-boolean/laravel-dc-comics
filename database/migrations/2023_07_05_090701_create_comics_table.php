821fc2a4ea3d6b952451c73247e56618da5fce60
vincenzodivita <divitavincenzo@icloud.com>
2023-07-05 11:29:22 +0200

    Add db fields

--- database/migrations/2023_07_05_090701_create_comics_table.php
+++ database/migrations/2023_07_05_090701_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
