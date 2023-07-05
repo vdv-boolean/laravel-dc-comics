78f5d7b0bd4ef0f8139ea8311564f69b5bd1a2fc
vincenzodivita <divitavincenzo@icloud.com>
2023-07-05 10:59:55 +0200

    Add shells

---
+++ shell/make.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+php artisan make:controller Guest/PageController
+php artisan make:controller Guest/$1\Controller
+
+php artisan make:model $1
+
+php artisan make:migration create_$2\s_table
